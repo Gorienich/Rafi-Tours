a506916679a65efd191d0efe7b3d736e7a15aaa5
Egor Grebenuk <107270919+Gorienich@users.noreply.github.com>
2024-10-25 05:16:02 +0300

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,32 @@
 // Wait until the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
+  // header
+      // Add event listeners after the header is loaded
+    const menuButton = document.getElementById("menu-button");
+    const closeButton = document.getElementById("close-button");
+    const menu = document.getElementById("menu");
+
+    menuButton.addEventListener("click", () => {
+      menu.classList.add("menu-active");
+      menu.classList.remove("menu-inactive");
+    });
+
+    closeButton.addEventListener("click", () => {
+      menu.classList.remove("menu-active");
+      menu.classList.add("menu-inactive");
+    });
+
+    // Close the menu when a link is clicked
+    const menuLinks = menu.querySelectorAll('a');
+    menuLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        menu.classList.remove("menu-active");
+        menu.classList.add("menu-inactive");
+      });
+    });
+
+
+  
   const sliderTeam = document.querySelector('.slider');
   const slidesTeam = document.querySelector('.slides');
   const slides = document.querySelectorAll('.slide');
